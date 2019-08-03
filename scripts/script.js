1f3c6e3fd7b9433edb6d353d65f27caa7ab821e6
Carol Park <carolpark09@gmail.com>
2019-08-02 23:19:30 -0300

    Add Responsive Menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
-document.getElementsByClassName('menuButton') {
-     
-}
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navMenu');
+  const navLinks = document.querySelectorAll('.navMenu li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('navActive');
+    console.log(nav.classList);
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
+      }
+    });
+  });
+  console.log('I\'m working!');
+};
+
+navSlide();
