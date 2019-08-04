3881d240723053670295c61479586b9afce10522
Carol Park <carolpark09@gmail.com>
2019-08-04 17:52:05 -0300

    Responsive Menu done

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,6 @@ const navSlide = () => {
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('navActive');
-    console.log(nav.classList);
 
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
@@ -14,8 +13,8 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
       }
     });
+    burger.classList.toggle('toggle');
   });
-  console.log('I\'m working!');
 };
 
 navSlide();
